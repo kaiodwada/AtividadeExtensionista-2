9f705536d1050b594ef0e018f76fcbc1b4e8d118
Kaioadwada <kaio-giovany@hotmail.com>
2026-06-22 22:26:07 -0300

    Pequenas correções

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,15 +3,12 @@ async function logout() {
     const urlAPILogout = 'http://localhost/ProjetoFinal/api/logout'
     const urlAPiHome = 'http://localhost/ProjetoFinal/'
     try {
-        // 1. Faz a requisição assíncrona (espera a resposta do servidor)
         const resposta = await fetch(urlAPILogout, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-
-        // Se a API der erro (ex: 404 ou 500), joga para o bloco catch
         if (!resposta.ok) {
             throw new Error('Erro ao buscar dados da API')
         } else {
